0c2e843c72357e0ffabdf68dbb11f34b0844d36d
Lene Omdal <lene.omdal@hotmail.com>
2021-11-08 10:56:26 +0100

    :art: Restructure the loading and checking data to a separate file

---
+++ load_data.R
@@ -0,0 +1,110 @@
+library(tidyverse)
+path = "\\\\fil.nice.ntnu.no\\nice\\p489\\miRNA 10 days\\data\\copies\\"
+
+#dir.exists(path)
+cpm.data = read.csv(paste0(path, "cpm_countMatrix_new data_subset.txt"), sep = "\t", dec= ",", header = TRUE, row.names = 1)
+colnames(cpm.data) = str_remove(colnames(cpm.data), "[X]")
+ncol(cpm.data)
+
+
+#Remove last two collumns?
+# this is only for full data set, not for the subset
+#cpm.data[,c(ncol(cpm.data)-1,ncol(cpm.data))]
+#mirna.data = subset(cpm.data, select = -c(ncol(cpm.data)-1, ncol(cpm.data)))
+#View(mirna.data)
+
+
+# For subset, just run the following
+mirna.data = cpm.data
+
+
+meta.data = read.csv(paste0(path, "Breastmilk_overview_10days.csv"), sep = ";", dec = ",", header = TRUE)
+class(meta.data)
+
+
+
+#extract 10 days samples
+ten.days.sample.nr = meta.data$bm_sample_no1[seq(1, length(meta.data$bm_sample_no1)-2)]
+ten.days.sample.nr = ten.days.sample.nr[toString(ten.days.sample.nr)]
+
+
+#Find only the ones included in miRNA sequencing project for 10 day samples, mirna = 1 in overview file
+
+ten.days.sample.included.nr = meta.data$bm_sample_no1[meta.data$mirna1 == 1]
+n.samples.10.days = length(ten.days.sample.included.nr)
+
+
+# Check that all samples are present
+for (i in seq(from = 1,to = length(ten.days.sample.included.nr))) {
+  if(toString(ten.days.sample.included.nr[i] %in% colnames(mirna.data))){
+    print("im good")
+  }
+  else {print(ten.days.sample.included.nr[i])}
+}
+
+
+
+
+
+# Define data frame containing only the samples we want
+ten.days.sample.df =  mirna.data[, as.character(ten.days.sample.included.nr)]
+
+
+
+#check for missing values
+apply(is.na(ten.days.sample.df), 2, which)
+sum(is.na(ten.days.sample.df))
+
+#data frame for only needed metadata
+
+ten.days.meta.data = meta.data[meta.data$mirna1 == 1,]
+
+
+
+#Explore data
+mirna.means = rowMeans(ten.days.sample.df)
+mirna.means[1]
+plot(mirna.means)
+hist(mirna.means)
+
+#Function for plotting histogram of expression levels in one miRNA
+
+class(t(ten.days.sample.df))
+
+hist_expression_level = function(dataframe, mirna){
+  hist(as.numeric(dataframe[mirna,]))
+}
+
+hist_expression_level2 = function(dataframe, mirna){
+  dataframe = as.data.frame(t(dataframe))
+  col = sym(colnames(dataframe)[mirna])
+  p = ggplot(data = dataframe, aes(x = !!col, y = ..density..)) + geom_histogram()
+  return(p)
+}
+hist_expression_level2(ten.days.sample.df, 1)
+
+
+
+#boxplot of most abundant miRNAs
+
+
+sum.reads = apply(ten.days.sample.df, 1, sum)
+
+sorted.sum.reads = sum.reads[order(sum.reads, decreasing = TRUE)]
+
+n.mirna.to.include = 20
+
+
+most.abundant.mirna.names = names(sorted.sum.reads[seq(1, n.mirna.to.include)])
+
+most.abundant.mirnas = ten.days.sample.df[most.abundant.mirna.names,]
+class(as.data.frame(t(most.abundant.mirnas)))
+
+abundant.mirna.long = pivot_longer(data = as.data.frame(t(most.abundant.mirnas)), cols = rownames(most.abundant.mirnas), names_to = "miRNA", values_to = "cpmValue")
+
+abundant.mirna.long$miRNA = factor(abundant.mirna.long$miRNA, levels = most.abundant.mirna.names)
+
+
+ggplot(data = abundant.mirna.long, aes(x = miRNA, y = cpmValue )) + geom_boxplot() + ylab("cpm value") + ggtitle("20 most abundant miRNAs") +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) 
+
