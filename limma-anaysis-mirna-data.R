c71fb685fde2013b216125c0056cd30c44053171
Lene Omdal <lene.omdal@hotmail.com>
2021-11-29 20:55:06 +0100

    :construction: test analysis including more covariates

--- limma-anaysis-mirna-data.R
+++ limma-anaysis-mirna-data.R
@@ -1,12 +1,13 @@
 library(edgeR)
+library(xtable)
+library(RColorBrewer)
 source("load_count_data.R")
 
 
 # Make dge object
 dge <- DGEList(counts=count.df)
 
-View(metadata.df)
-
+# Make groups
 treatment = ifelse(metadata.df$probiotic ==1, "P", "nP")
 outcome = ifelse(metadata.df$ad == 1, "AD", "nAD")
 
@@ -14,6 +15,8 @@ groups_p = as.factor(treatment)
 groups = as.factor(paste0(treatment,"_", outcome))
 groups_ad = as.factor(outcome)
 
+
+# Assign groups to dge.g
 dge.g = dge
 dge.g$samples$group = groups
 
@@ -25,7 +28,7 @@ summary(lcpm)
 
 
 # TABLE: not expressed miRNAs
-table(rowSums(dge$counts==0)==60)
+table.zero.expression = table(rowSums(dge$counts==0)==60)
 
 
 
@@ -35,16 +38,13 @@ dge.g <- dge[keep.exprs,, keep.lib.sizes=FALSE]
 dim(dge.g$counts)
 
 
-
-# design matrix
+# second way of filtering, using design matrix
 design = model.matrix(~probiotic*ad, data = metadata.df)
 head(design)
-
-
-# second way of filtering????????
 keep <- filterByExpr(dge, design = design)
 dge <- dge[keep,,keep.lib.sizes=FALSE]
 dim(dge$counts)
+
 
 
 # check that design matrix gives same result as groups
@@ -55,59 +55,33 @@ groups == groups_design
 
 
 
-# check which miRNA are different between the two filterings
-diff.index = c()
-for(i in 1:nrow(dge.g$counts)){
-  if(rownames(dge.g$counts)[i] %in% rownames(dge$counts)){
-    next
-  }
-  else{diff.index = c(diff.index, which(rownames(dge.g$counts) == rownames(dge.g$counts)[i]))}
-}
-diff.index
-# ER DET RART AT ALLE DISSE BLIR TATT BORT?
-dge.g$counts[diff.index,]
-
-
-#Check if some a filtered from dge.g and not dge
-dim(dge.g$counts[diff.index,])
-dim(dge.g$counts)[1] - dim(dge$counts)
-
-for(i in 1:nrow(dge$counts)){
-  if(rownames(dge$counts)[i] %in% rownames(dge.g$counts)){
-    next
-  }
-  else{print(rownames(dge$counts)[i])}
-}
-
-# eventuell filtrering?
-
-#keep.modified <- filterByExpr(dge, design = design, min.count = 0, min.total.count = 60*3 )
-#dge.filtered <- dge[keep.modified,,keep.lib.sizes=FALSE]
-#dim(dge.filtered$counts)
-#dim(dge)
-
-
-
-#Optional? normalization
+#Optional? normalization. Not used here
 # dge = calcNormFactors(dge, method = "TMM")
 
 
-# Plot 
-library(RColorBrewer)
+#MDS Plot
+dge.g$samples$group = groups_p
+lcpm = cpm(dge.g, log =  TRUE) 
 
-
-col.group = groups       # or= groups     # or = groups_p
+col.group = groups_p       # or= groups     # or = groups_p
 levels(col.group) =  brewer.pal(nlevels(col.group), "Set1")
 col.group = as.character(col.group)
-plotMDS(lcpm, labels=groups, col=col.group)
+plotMDS(lcpm, labels=groups_p, col=col.group)
 title(main="MDS plot")
+
+#save plot
+#svg("C:\\Users\\Lene\\Documents\\Skole\\Prosjektoppgave\\project-thesis-mirna\\plots\\MDS-plot.svg")
+#plotMDS(lcpm, labels=groups_p, col=col.group)
+#title(main="MDS plot")
+#legend("topleft", as.character(unique(dge.g$samples$group)),col = unique(col.group) , pch = 20)
+#dev.off() 
+
+
 
 
 
 # Define design matrix
-
 design.matrix = model.matrix(~probiotic + ad , data = metadata.df)
-
 
 
 # design matrix using the groups
@@ -115,7 +89,7 @@ design.matrix.g = model.matrix(~0+groups)
 colnames(design.matrix.g) <- gsub("groups", "", colnames(design.matrix.g))
 
 
-#Make contrast matrix
+#Make contrast matrix, denne er feil nå som interaction er med. MÅ ENDRES
 colnames(design.matrix)[1] = "intercept"
 contr.matrix <- makeContrasts(
   PvsnP = probiotic,
@@ -133,6 +107,8 @@ contr.matrix
 contr.matrix.g <- makeContrasts(
   PnADvsnPAD = P_nAD - nP_AD,
   PvsnP = 0.5*(P_AD+P_nAD)-0.5*(nP_AD+nP_nAD),
+  ADvsnAD = 0.5*(P_AD + nP_AD) - 0.5*(P_nAD + nP_nAD),
+  PnADvsAll = P_nAD - (P_AD + nP_nAD + nP_AD)/3,
   levels = colnames(design.matrix.g))
 
 contr.matrix.g
@@ -141,12 +117,12 @@ contr.matrix.g
 
 # VOOM METHOD
 
-voom.weights = voom(dge, design.matrix, plot=TRUE)
+#voom.weights = voom(dge, design.matrix, plot=TRUE)
 voom.weights.g = voom(dge.g, design.matrix.g, plot = TRUE) 
 
 #save plot
 svg("C:\\Users\\Lene\\Documents\\Skole\\Prosjektoppgave\\project-thesis-mirna\\plots\\mean-variance-trend.svg")
-v = voom(dge, design.matrix, plot=TRUE)
+v = voom(dge.g, design.matrix.g, plot=TRUE)
 # Close the graphics device
 dev.off() 
 
@@ -156,11 +132,13 @@ eB.fit = eBayes(voom.fit)
 
 voom.fit.g = lmFit(voom.weights.g, design.matrix.g)
 voom.fit.g = contrasts.fit(voom.fit.g, contrasts = contr.matrix.g)
-eB.fit.g = eBayes(voom.fit.g)
+eB.fit.g = eBayes(voom.fit.g, trend = FALSE)
+
 #save plot
 svg("C:\\Users\\Lene\\Documents\\Skole\\Prosjektoppgave\\project-thesis-mirna\\plots\\final-MV-trend.svg")
 plotSA(eB.fit.g, main="Final model: Mean-variance trend")
 dev.off()
+
 
 
 # table for quick comparison, adjusted p-value cutoff is set at 5% by default
@@ -172,13 +150,15 @@ dt <- decideTests(tfit)
 summary(dt)
 
 
-#Examining individual miRNA
-
+# Results by Voom
 voom.PvsnP = topTable(fit = eB.fit, coef = 1)
 voom.PvsnP
 
 voom.PnADvsnPAD.g = topTable(fit = eB.fit.g, coef = 1)
-voom.PvsnP.g
+voom.PnADvsnPAD.g
+xtable(voom.PnADvsnPAD.g %>% slice_head(n = 10))
+
+voom.PnADvsAll = topTable(fit = eB.fit.g, coef = 4)
 
 plotMD(tfit, column=1, status=dt[,1], main=colnames(tfit)[1], 
        xlim=c(-8,13))
@@ -188,12 +168,31 @@ plotMD(tfit, column=1, status=dt[,1], main=colnames(tfit)[1],
 
 lpcm = cpm(dge.g, log = TRUE)
 limma.fit = lmFit(lcpm, design.matrix.g)
-limma.fit <- eBayes(limma.fit, trend=TRUE)
 limma.fit.contr = contrasts.fit(limma.fit, contrasts = contr.matrix.g)
 limma.fit.contr = eBayes(limma.fit.contr, trend = TRUE)
-topTable(limma.fit.contr, coef = 1)
+limma.PnADvsnPAD = topTable(limma.fit.contr, coef = 1)
+limma.PnADvsnPAD
+
+xtable(limma.PnADvsnPAD %>% slice_head(n = 10))
+
+limma.PnADvsAll = topTable(limma.fit.contr, coef = 4)
+
+
+#Including more covariates
+
+design.matrix.full = model.matrix(~0+groups + sex + sib + matatopy, data = metadata.df)
+design.matrix.full
+
+voom.full = voom(dge.g, design.matrix.full, plot = TRUE) 
+voom.fit.full = lmFit(voom.full, design.matrix.g)
+voom.fit.full = contrasts.fit(voom.fit.full, contrasts = contr.matrix.g)
+eB.fit.full = eBayes(voom.fit.full, trend = FALSE)
+
+plotSA(eB.fit.full, main="Final model: Mean-variance trend")
+summary(decideTests(eB.fit.full))
+
+topTable(eB.fit.full, coef = 1)
 
 
 
-
-
+#SJEKK COVARIATENE OG DERES SIGNIFIKANS
